24adc95e2e18ce6245089f92ed50aa3396dc4e2e
Emin-ACIKGOZ <eminsalihacikgoz@gmail.com>
2025-11-06 19:10:18 +0300

    feat: Add pre-flight check for external binaries

---
+++ internal/preflight/checks.go
@@ -0,0 +1,16 @@
+package preflight
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// CheckBinaries ensures all required binaries are in the user's $PATH.
+func CheckBinaries(binaries ...string) error {
+	for _, bin := range binaries {
+		if _, err := exec.LookPath(bin); err != nil {
+			return fmt.Errorf("'%s' command not found in $PATH. Please install it to continue", bin)
+		}
+	}
+	return nil
+}
